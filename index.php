c9e1d205689cee9b5bcbe6dff26b81b20d9d5b45
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-11-24 12:12:38 -0300

    Last searches added

--- index.php
+++ index.php
@@ -5,6 +5,39 @@ if (!empty($_GET['q'])) {
     $artist_url = 'https://rest.bandsintown.com/artists/' . $_GET['q'] . '/';
     $artist = json_decode(file_get_contents($artist_url . '?app_id=' . APP_ID));
 }
+
+/**
+ * Handle search history
+ */
+session_start();
+if (!empty($artist)) {
+    $artist->term = $_GET['q'];
+
+    if (!empty($_SESSION['history'])) {
+        /**
+         * Remove artist from history if already there
+         */
+        foreach ($_SESSION['history'] as $key => $value) {
+            if ($value->id == $artist->id) {
+                unset($_SESSION['history'][$key]);
+            }
+        }
+
+        /**
+         * Limit the number of previous artists to 3
+         * The array will actualy have 4 artists, but since the last element is the current artist
+         * we don't need to show it, so just need to show to first 3 items
+         */
+        if (count($_SESSION['history']) > 3) {
+            $_SESSION['history'] = array_slice($_SESSION['history'], 1, 3);
+        }
+    }
+
+    /**
+     * Add user to the end of the history list
+     */
+    $_SESSION['history'][] = $artist;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
@@ -14,26 +47,42 @@ if (!empty($_GET['q'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Vanhackaton 4.0 - Frontend Challenge</title>
         <link rel="shortcut icon" href="img/favicon.ico">
-        <meta name="author" content="Luciano Garcia Bes">
+        <meta name="author" content="Alex Camargo - Luciano Garcia Bes">
         <meta name="description" content="Solution for frontend challenge in Vanhackaton 4.0">
         <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700%7CMontserrat:400,700%7CRaleway:300,400,600" rel="stylesheet">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/fonts.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/owl.carousel.min.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/styles.css">
     </head>
-    <body>
+    <body onLoad="initMap();">
         <div id="wrapper">
             <div class="header-transparent header-transparent-light menu-fixed-dark menu-dark-mobiles xs-menu-wrapper-dark">
                 <header class="header-wrapper">
                     <div class="megamenu">
                         <div class="row">
-                            <div class="col-12">
+                            <div class="col-sm-8 col-md-9">
                                 <form method="GET" class="menu-search">
                                     <input type="text" name="q" placeholder="Search">
                                     <button type="submit" class="default"><i class="icon icon_search"></i></button>
                                 </form>
+                            </div>
+                            <div class="col-sm-4 col-md-3">
+<?php
+/**
+ * If have previous searches, show history
+ */
+if (!empty($_SESSION['history'])) {
+    foreach (array_slice($_SESSION['history'], 0, count($_SESSION['history']) - 1) as $history) {
+?>
+                                <div class="history">
+                                    <a href="./?q=<?php echo($history->term); ?>"><img src="<?php echo($history->thumb_url); ?>"></a>
+                                </div>
+<?php
+    }
+}
+?>
                             </div>
                         </div>
 <?php
@@ -53,7 +102,7 @@ if (!empty($_GET['q']) and empty($artist)) {
                 <div class="bg-overlay gradient-1"></div>
                 <div class="container">
                     <div class="row mt50 mb50">
-                        <div class="col-sm-8 col-sm-offset-2 text-center">
+                        <div class="col-sm-12 col-sm-offset-2 text-center">
 <?php
 if (empty($artist)) {
 ?>
@@ -101,23 +150,26 @@ if (!empty($artist)) {
             $tickets = FALSE;
             foreach ($event->offers AS $offer) {
                 if ($offer->type == 'Tickets' && $offer->status == 'available') {
-                    $tickets = TRUE;
+                    $tickets = $offer->url;
                 }
             }
 ?>
-                            <div class="col-md-4 box-services-2">
+                            <div class="col-md-4 event" style="height:170px;">
                                 <div class="br-bottom mt40 mb0"></div>
                                 <div style="position:relative;">
                                     <h3 class="title-small">
 <?php
-        echo(date(TIME_FORMAT, strtotime($event->datetime)));
+        $date = date(TIME_FORMAT, strtotime($event->datetime));
+        $location = $event->venue->city . ', ' . (!ctype_digit($event->venue->region) ? $event->venue->region . ', ' : '') . $event->venue->country;
+
+        echo($date);
         if ($tickets) {
             echo(' <span class="fa fa-ticket" style="color:#f00;font-size:12px;"></span>');
         }
 ?>
                                     </h3>
-                                    <a data-toggle="modal" data-target=".bd-example-modal-lg" OnClick="initMap(<?php echo($event->venue->latitude)  ?>,<?php echo($event->venue->longitude)  ?>)" ><p><?php echo(strtoupper($event->venue->name)); ?> Latitude <?php echo($event->venue->latitude)  ?>  Longitude <?php echo($event->venue->longitude)  ?> </p></a>
-                                    <p><?php echo($event->venue->city . ', ' . (!ctype_digit($event->venue->region) ? $event->venue->region . ', ' : '') . $event->venue->country); ?></p>
+                                    <a href="#" class="show-event" data-date="<?php echo($date); ?>" data-venue="<?php echo(strtoupper($event->venue->name)); ?>" data-location="<?php echo($location); ?>" data-latitude="<?php echo($event->venue->latitude); ?>" data-longitude="<?php echo($event->venue->longitude); ?>" data-tickets="<?php echo($tickets); ?>" ><p><?php echo(strtoupper($event->venue->name)); ?></p></a>
+                                    <p><?php echo($location); ?></p>
                                 </div>
                             </div>
 <?php
@@ -131,7 +183,7 @@ if (!empty($artist)) {
 <?php
 }
 ?>
-            <footer class="footer-wrapper footer-background style-2 bg-img bg07 stellar" data-stellar-background-ratio="0.4">
+            <footer class="footer-wrapper footer-background style-2 bg-img stellar" data-stellar-background-ratio="0.4">
                 <div class="sub-footer">
                     <div class="container">
                         <div class="row">
@@ -144,29 +196,60 @@ if (!empty($artist)) {
             </footer>
         </div>
         <!-- Modal Maps -->
-       <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+       <div id="modal-event" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
+                    <h1 id="modal-event-title">VENUE NAME</h1>
+                    <p id="modal-event-location">City, State, Country</p>
                     <div id="map"></div>
                 </div>
             </div>
         </div>
         <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <script src="js/plugins.min.js"></script>
-        <script src="js/jquery.nav.min.js"></script>
-        <script src="js/owl.carousel.min.js"></script>
-        <script src="js/main.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAm3KarFLFFjlDCuVDIcixLRhQ-ANyGwAc"></script>
         <script src="js/scripts.js"></script>
         <script>
-            function initMap(latitude,longitude) {
-                var map;
+            var map;
+            var marker;
+            var latlng;
+
+            function initMap(lat = 0, lng = 0, venue = '') {
+                latlng = new google.maps.LatLng(lat, lng);
+
                 map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: latitude, lng: longitude},
-                zoom: 8
+                    enter: latlng,
+                    zoom: 5
+                });
+
+                marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                    title: venue
                 });
             }
+
+            $(function() {
+                $('.show-event').click(function(e) {
+                    e.preventDefault();
+
+                    latlng = new google.maps.LatLng($(this).data('latitude'), $(this).data('longitude'));
+
+                    $('#modal-event-title').text($(this).data('venue'));
+                    $('#modal-event-location').text($(this).data('location'));
+                    map.setCenter(latlng);
+                    marker.setPosition(latlng);
+
+                    $('#modal-event').modal('show');
+                });
+
+                /** Fix for Google Maps in dynamic elements **/
+                $('#modal-event').on('shown.bs.modal', function() {
+                    google.maps.event.trigger(map, "resize");
+                    map.setCenter(latlng);
+                })
+            });
         </script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAm3KarFLFFjlDCuVDIcixLRhQ-ANyGwAc" async defer></script>
     </body>
 </html>
